de3d5f56995341ab5e90911819b7a0d1f60e7f65
Techno857 <angel.feliciano21@upr.edu>
2023-11-12 13:35:44 -0400

    Unfinished pistol implementation @YR

--- Goon/goon.gd
+++ Goon/goon.gd
@@ -4,7 +4,8 @@ extends CharacterBody2D
 @onready var animations = $AnimationPlayer
 @onready var direction = "right"
 
-
+var bullet_speed = 1000
+var bullet = preload("res://World/Bullet.tscn")
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
@@ -42,6 +43,16 @@ func _physics_process(_delta):
 	singleAnimation()
 	look_at(get_global_mouse_position())
 
+	if Input.is_action_just_pressed("LMB"):
+		fire()
+
 func _on_hurt_box_area_entered(area):
 	if area.has_method("openDoor") && area.doorClosed:
 		area.openDoor()
+
+func fire():
+	var bullet_instance = bullet.instantiate() 
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child",bullet_instance)
