6bb7764018f84c0e562003f843c800435011f415
Techno857 <angel.feliciano21@upr.edu>
2023-11-13 02:22:51 -0400

    Added knife collission detection + weapon sounds

--- Goon/goon.gd
+++ Goon/goon.gd
@@ -30,27 +30,11 @@ func updateAnimation():
 		$GoonGunCollision.hide()
 		$Goon.show()
 		$GoonCollision.show()
-	
-
-	# # Diagonal movements
-	# if  velocity.x < 0 && velocity.y < 0: direction = "up_left"
-	# elif  velocity.x < 0 && velocity.y > 0: direction = "down_left"
-	# elif  velocity.x > 0 && velocity.y < 0: direction = "up_right"
-	# elif  velocity.x > 0 && velocity.y > 0: direction = "down_right"
-	
-	# # Cardinal movements
-	# elif velocity.x < 0: direction = "left"
-	# elif  velocity.x > 0: direction = "right"
-	# elif  velocity.y < 0: direction = "up"
-	# elif  velocity.y > 0: direction = "down"
-
-	# animations.play("goon_walk_"+ direction)
 
 func _physics_process(_delta):
 
 	handleInput()
 	move_and_slide()
-	# updateAnimation()
 	updateAnimation()
 	look_at(get_global_mouse_position())
 	
@@ -62,12 +46,16 @@ func _physics_process(_delta):
 		pistol.gunAcquired = false
 		
 	if Input.is_action_just_pressed("RMB") && pistol.gunAcquired == false:
-		_on_knife_timer_timeout()
-
-func _on_hurt_box_area_entered(area):
-	if area.has_method("openDoor") && area.doorClosed:
-		area.openDoor()
-
+		knifeTimer.start()
+		knife.visible = true
+		knife.set_collision_mask_value(1, true)
+		$KnifeStab.play()
+		
+		await knifeTimer.timeout
+		
+		knife.set_collision_mask_value(1, false)
+		knife.visible = false
+		
 func fire():
 	var bullet_instance = bullet.instantiate() 
 	bullet_instance.position = get_global_position()
@@ -75,10 +63,12 @@ func fire():
 	bullet_instance.rotation_degrees = rotation_degrees
 	bullet_instance.linear_velocity = Vector2(bullet_speed,0).rotated(rotation)
 	get_tree().get_root().call_deferred("add_child",bullet_instance)
+	$SuppGunshot.play()
+	
+func _on_hurt_box_area_entered(area):
+	if area.has_method("openDoor") && area.doorClosed:
+		area.openDoor()
 
-
-func _on_knife_timer_timeout():
-	knifeTimer.start()
-	knife.visible = true
-	await knifeTimer.timeout
-	knife.visible = false
+func _on_knife_area_entered(area):
+	if area.has_method("openDoor") && area.doorClosed:
+		area.openDoor()
